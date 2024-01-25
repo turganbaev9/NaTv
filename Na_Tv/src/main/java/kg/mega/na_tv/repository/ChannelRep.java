764bae0d3734daca514224b4ddc30a66dc9d27e3
Bektursun_K <beka.turganbaev98@gmail.com>
2024-01-25 23:54:38 +0300

    Created repositoryes

---
+++ Na_Tv/src/main/java/kg/mega/na_tv/repository/ChannelRep.java
@@ -0,0 +1,17 @@
+package kg.mega.na_tv.repository;
+
+import model.Channel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChannelRep extends JpaRepository<Channel,Long> {
+ /*   @Query(value = "select * from tb_channel" +
+            "where channel_status=true" +
+            "order by channel_name asc", nativeQuery = true)
+    List<Channel> findAllByActive();
+
+    @Query(value = "select channel_status from tb_channel"+
+            "where id =:id ",nativeQuery = true)
+    boolean findByActive(Long id);*/
+}
