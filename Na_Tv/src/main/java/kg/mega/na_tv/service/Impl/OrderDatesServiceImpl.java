31789b7c5ec26d1e1c6deab3f2b0f56c5b3ffd22
Bektursun_K <beka.turganbaev98@gmail.com>
2024-01-26 14:36:01 +0300

    created OrderDatesServiceImpl

--- Na_Tv/src/main/java/kg/mega/na_tv/service/Impl/OrderDatesServiceImpl.java
+++ Na_Tv/src/main/java/kg/mega/na_tv/service/Impl/OrderDatesServiceImpl.java
@@ -1,15 +1,22 @@
 package kg.mega.na_tv.service.Impl;
 
+import kg.mega.na_tv.mapper.OrderDatesMapper;
+import kg.mega.na_tv.repository.OrderDatesRep;
 import kg.mega.na_tv.service.OrderDatesService;
 import lombok.RequiredArgsConstructor;
+import model.OrderDates;
 import model.dto.OrderDatesDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class OrderDatesServiceImpl implements OrderDatesService {
+    private final OrderDatesRep orderDatesRep;
+
     @Override
-    public OrderDatesDTO save(OrderDatesDTO orderDates) {
+    public OrderDatesDTO save(OrderDatesDTO orderDatesDTO) {
+
+        OrderDates orderDate = OrderDatesMapper.INSTANCE.toEntity(orderDatesDTO);
         return null;
     }
 }
