34356ec184fc2a2c37d5b9ec804493cb48412171
Bektursun_K <beka.turganbaev98@gmail.com>
2024-01-28 01:13:11 +0300

    created PriceServiceImpl

--- Na_Tv/src/main/java/kg/mega/na_tv/service/Impl/PriceServiceImpl.java
+++ Na_Tv/src/main/java/kg/mega/na_tv/service/Impl/PriceServiceImpl.java
@@ -1,20 +1,54 @@
 package kg.mega.na_tv.service.Impl;
 
+import kg.mega.na_tv.mapper.PriceMapper;
+import kg.mega.na_tv.repository.DiscountRep;
+import kg.mega.na_tv.repository.PriceRep;
+import kg.mega.na_tv.repository.TextRep;
+import kg.mega.na_tv.service.DiscountService;
 import kg.mega.na_tv.service.PriceService;
 import lombok.RequiredArgsConstructor;
+import model.Discount;
 import model.Price;
+import model.Text;
 import model.dto.PriceDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
+
 @Service
 @RequiredArgsConstructor
 public class PriceServiceImpl implements PriceService {
+    private  final PriceRep priceRep;
+    private  final TextRep textRep;
+    private final DiscountService discountService;
+    private  final DiscountRep discountRep;
     @Override
     public PriceDTO save(PriceDTO priceDTO) {
-        return null;
-    }
+        Price price= PriceMapper.INSTANCE.toEntity(priceDTO);
+        Text text= textRep.findById(priceDTO.getTextDTO().getId()).orElseThrow(()
+                -> new NoSuchElementException("Text with ID not found"));
+        Discount discount=discountRep.findById(priceDTO.getDiscountDTO().getId()).orElseThrow(()
+                ->new NoSuchElementException("Discount with ID not found"));
+        Double totalPrice = countPrice(Collections.singletonList(text),price);
+        price.setDiscount(discount);
+        price.setText(text);
+        price.setPrice(totalPrice);
+        priceRep.save(price);
+        return PriceMapper.INSTANCE.toDTO(price);
+    }  public Double countPrice(List<Text> texts,Price price){
 
+        double pricePerSymbol=price.getPricePerSimbol();
+        double totalPrice=0;
+        pricePerSymbol=price.getPricePerSimbol();
+        for (Text text : texts) {
+            int symbolCount= text.getSymbolCount();
+            double textPrice=symbolCount*pricePerSymbol;
+            totalPrice+=textPrice;
+        }
+        return totalPrice;
+    }
     @Override
     public List<Price> findAll() {
         return null;
