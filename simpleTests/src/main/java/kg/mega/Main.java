7e035d345cb9ca24e3992e5f35346000b57781c6
Bektursun_K <beka.turganbaev98@gmail.com>
2024-02-23 10:49:35 +0300

    простой массив программа выводящяя схожие числа! и количество повторений

--- simpleTests/src/main/java/kg/mega/Main.java
+++ simpleTests/src/main/java/kg/mega/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
       //простой массив программа выводящяя схожие числа! и количество повторений
         int[] nums = {1, 3, 4, 5, 5, 4, 66, 77, 87, 66, 4};
-
         for (int i = 0; i < nums.length; i++) {
             int quantityCiphers = 0;  // Сбрасываем количество повторений для каждого числа
             for (int j = i + 1; j < nums.length; j++) {
---
+++ simpleTests/src/main/java/kg/mega/test1/test1.java
@@ -0,0 +1,7 @@
+package kg.mega.test1;
+
+public class test1 {
+    public static void main(String[] args) {
+
+    }
+}
